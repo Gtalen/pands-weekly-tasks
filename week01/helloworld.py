24ff5a5c5b94bba4c1a4bd2f24e7ef5cb9cd6fdb
Gtalen <great.talen@gmail.com>
2024-02-26 07:42:49 +0000

    addon

---
+++ week01/helloworld.py
@@ -0,0 +1,4 @@
+#helloworld.py
+#this program prints hello world
+#author Ebelechukwu Igwagu
+print("Hello World!")
---
+++ week02/hello.py
@@ -0,0 +1,4 @@
+#hello.py
+#exploring python variables and operators
+#Ebelechukwu Igwagu
+print ("Hello")
