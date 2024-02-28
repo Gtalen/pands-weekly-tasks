f78c90908ecfceff0ce49eea4d7ae5c1e6c74794
Gtalen <great.talen@gmail.com>
2024-02-28 14:36:32 +0000

    learning input and arithmetic operators

---
+++ week02/bank.py
@@ -0,0 +1,9 @@
+#weekly task 2
+#Applying the input function and arithmetic operators
+#Author: ebelechukwu Igwagu
+item1 = input ("Enter cost for item 1 in cent: ")
+item2 = input ("Enter cost for item 2 in cent: ")
+total = int(item1) + int(item2)
+print (total/100)
+
+
