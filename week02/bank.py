710354015f7f324d50168ad346a0001b14353457
Gtalen <great.talen@gmail.com>
2024-02-29 07:47:29 +0000

    euro sign addition

--- week02/bank.py
+++ week02/bank.py
@@ -4,6 +4,26 @@
 item1 = input ("Enter cost for item 1 in cent: ")
 item2 = input ("Enter cost for item 2 in cent: ")
 total = int(item1) + int(item2)
-print (total/100)
+euro =  (total/100)
+print ("€{:,}".format(euro))
+#using the locale function to set the currency used in a region
+import locale
+print(locale.setlocale(locale.LC_ALL, ''))
+print(locale.setlocale(locale.LC_ALL, 'en_150'))
+item1 = int (input ("Enter cost for item 1 in cent: "))
+item2 = int (input ("Enter cost for item 2 in cent: "))
+total = item1 + item2
+euro = (total/100)
+print ("€{:,}".format(euro))
 
 
+
+
+
+
+
+
+#References
+#https://docs.python.org/3/library/locale.html
+#https://stackabuse.com/format-number-as-currency-string-in-python/
+#https://learn.microsoft.com/en-us/openspecs/windows_protocols/ms-lcid/70feba9f-294e-491e-b6eb-56532684c37f
