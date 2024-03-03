614cf93b03e779708d28aed8398b5ebbcc5becc7
Gtalen <great.talen@gmail.com>
2024-03-03 00:47:33 +0000

    addon

---
+++ week03/accounts.py
@@ -0,0 +1,18 @@
+#variables
+#application of slicing in python codes
+#Author: Ebelechukwu Igwagu
+accountno = input('Enter your 10 digit account number: ')
+accno = accountno[6:]
+print (accno)
+display = print ("XXXXXX"+accno)
+account = input("Enter your account number: ")
+acc = account[-4:]
+acc_display = print (f"XXXXXXX"+acc)
+
+
+
+
+
+
+#References
+#https://www.w3schools.com/python/python_strings_slicing.asp
