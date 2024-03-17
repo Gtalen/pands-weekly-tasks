6ceee928d376a954d32164d9d3d2541a176dae63
Gtalen <great.talen@gmail.com>
2024-03-17 19:15:27 +0000

    modified

--- week04/collatz.py
+++ week04/collatz.py
@@ -4,26 +4,29 @@
 #prints off the number sequence using the calculation below
 #Author: Ebelechukwu Igwagu
 
+#creates an empty list
+numbers = []
+
 #The line below prompts the  user for a number input
 number = int(input("Enter a positive number: "))
-
 
 #ensures that only a positive integer is entered
 while number <= 0:
     print ("Please enter a positive integer: ")
     number = int(input("Enter a positive number: "))
-#stores the number in a list
-numbers = [number]
 
-#defines the condition statement
-while number != 1:
-    print (number)
+#appends the initial number to the list
+numbers.append(number) 
+
+#defines the condition statement for generating the collatz sequence
+while number != 1:    
     if  number % 2 == 0:    #applies to even numbers
        number = number// 2
     else:                   #applies to odd numbers
-        number = ((number * 3) + 1)/2
-        numbers.append(number) #appends number to list
-        print (numbers)
-print (1)
+        number = (number * 3 + 1)
+    numbers.append(number) #appends current number to the list
+        
+print (numbers)
+
 
 
